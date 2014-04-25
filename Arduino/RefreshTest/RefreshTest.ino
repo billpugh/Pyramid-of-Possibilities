515483f6614b83732f8167dbd317c2fe5d4bb74d
Bill Pugh <pugh@cs.umd.edu>
2014-04-25 13:05:01 -0400

    Fix comments

--- Arduino/RefreshTest/RefreshTest.ino
+++ Arduino/RefreshTest/RefreshTest.ino
@@ -1,14 +1,4 @@
-/* PP0
- First version of code for LED lighting on the pyramid of possibilities.
- 
- Using MMA8452Q accelerometer and neopixel lighting.
- 
- Based on MMA8452Q Example Code by: Jim Lindblom
- 
- Note: The MMA8452 is an I2C sensor, however this code does
- not make use of the Arduino Wire library. Because the sensor
- is not 5V tolerant, we can't use the internal pull-ups used
- by the Wire library. Instead use the included i2c.h, defs.h and types.h files.
+/* Refresh test
  */
 
 #include <Adafruit_NeoPixel.h>
@@ -170,64 +160,3 @@ void loop()
 
 
 }
-
-uint32_t color(float r, float g, float b) {
-  return strip.Color((int)(255*r), (int) (255*g), (int)(255*b));
-}
-
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t HSVtoRGB(  float h, float s, float v )
-{
-  int i;
-  float r,g,b;
-  float f, p, q, t;
-  if( s == 0 ) {
-    // achromatic (grey)
-    return color(v,v,v);
-  }
-  h /= 60;			// sector 0 to 5
-  i = floor( h );
-  f = h - i;			// factorial part of h
-  p = v * ( 1 - s );
-  q = v * ( 1 - s * f );
-  t = v * ( 1 - s * ( 1 - f ) );
-  switch( i ) {
-  case 0:
-    return color(v,t,p);
-
-  case 1:
-    return color(q,v,p);
-
-  case 2:
-    return color(p,v,t);
-
-  case 3:
-    return color(p,q,v);
-
-  case 4:
-    return color(t,p,v);
-
-  case 5:
-  default:
-    return color(v,p,q);
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
