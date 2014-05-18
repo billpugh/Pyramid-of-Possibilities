407589cdf83194da12152d61bb342b489424df35
Bill Pugh <pugh@cs.umd.edu>
2014-05-18 11:05:58 -0400

    Add blinking lights

--- Arduino/T3_readmac/T3_readmac.ino
+++ Arduino/T3_readmac/T3_readmac.ino
@@ -3,17 +3,25 @@
 void setup() {
   delay(1000);
   Serial.begin(57600);
-  
+
   Serial.println("Reading MAC from hardware...");
   read_mac();
-  
+
   Serial.print("MAC: ");
   print_mac();
   Serial.println();
-  
+  pinMode(13, OUTPUT); 
+
+
   Serial.print("Finished.");
 }
 
 void loop() {
+  digitalWrite(13, HIGH); 
+  delay(500);
+  digitalWrite(13, LOW); 
+  delay(500);
+
 }
+
 
