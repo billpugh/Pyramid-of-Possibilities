f942516b9e558213679c0911cc3ae1eae2e63dc2
Bill Pugh <pugh@cs.umd.edu>
2014-07-16 23:33:59 -0400

    Updates to platform data

--- PlatformData/Platform.h
+++ PlatformData/Platform.h
@@ -10,5 +10,24 @@
 #define __PlatformData__Platform__
 
 #include <iostream>
+#include <stdint.h>
+
+class Platform {
+public:
+    const uint8_t tier;
+    const uint8_t number;
+    const uint8_t identifier;
+    const int16_t x,y,z;
+    
+    Platform(
+             uint8_t tier,
+             uint8_t number,
+             uint8_t identifier,
+             int16_t x,
+             int16_t y,
+             int16_t z) : tier(tier), number(number), identifier(identifier), x(x), y(y), z(z) {};
+    
+    
+};
 
 #endif /* defined(__PlatformData__Platform__) */
