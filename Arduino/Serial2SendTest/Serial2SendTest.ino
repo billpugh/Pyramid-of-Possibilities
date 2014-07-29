e44ba64ad0ad748e36b38a6cd1b9a5c6c7e04b72
Bill Pugh <pugh@cs.umd.edu>
2014-07-29 10:52:53 -0400

    Tweaks to serial testing code

--- Arduino/Serial2SendTest/Serial2SendTest.ino
+++ Arduino/Serial2SendTest/Serial2SendTest.ino
@@ -11,12 +11,15 @@ void setup()
   pinMode(led, OUTPUT);  
   pinMode(rxPin, INPUT_PULLUP);
   pinMode(txPin, OUTPUT); 
-  Serial2.begin(57600); 
+  Serial2.begin(115200); 
+ //  Serial2.begin(9600); 
 
+  delay(2000);
+  Serial.begin(9600);
+  Serial.println("Serial2SendTest");
   // RS485 RTS handshake signal setup
   pinMode(ctsPin, OUTPUT);
   CORE_PIN22_CONFIG = PORT_PCR_MUX(3);	// Set UART1_RTS function (ALT3, see page 209)
-  //	UART1_MODEM = 0x02;			// Set TXRTSE enable RTS on transmit with active LOW
   UART1_MODEM = 0x06;			// Set TXRTSE enable RTS on transmit with active HIGH
 }
 
@@ -24,10 +27,12 @@ void setup()
 void loop() {
   int now = millis() / 1000;
   Serial2.println(now);
+  Serial.println(now);
   digitalWrite(led, HIGH);
   delay(500);
   digitalWrite(led, LOW);
   delay(500);
 }
+
 
 
