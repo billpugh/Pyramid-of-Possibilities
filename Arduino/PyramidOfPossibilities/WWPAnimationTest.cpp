45fbd66d50347da8c13604f9b4f17cbfda1c0b31
Bill Pugh <pugh@cs.umd.edu>
2014-08-01 08:22:34 -0400

    First draft of flash attach

--- Arduino/PyramidOfPossibilities/WWPAnimationTest.cpp
+++ Arduino/PyramidOfPossibilities/WWPAnimationTest.cpp
@@ -8,21 +8,26 @@
 #include "WWPAnimationTest.h"
 
 void WWPAnimationTest::paint(RNLights & lights) {
-  
-      int value = 255 - info.timeSinceLastTap();
-      if (value < 20)
-        value = 20;
-      lights.setAllPixelHSVs(hue,255,value);
-//  
-//    uint16_t length = (getAnimationMillis() / 10) % (info.numLEDs*2);
-//    if (length >= info.numLEDs)
-//      length = info.numLEDs*2-1-length;
-//    if (length >=  info.numLEDs | length < 0)
-//      length = 0;
-//    for(int i = 0; i < length; i++)
-//      lights.setPixelColor(i, 64, 64, 64);
-   }
 
-  char * WWPAnimationTest:: name() {
-    return "WWPAnimationTest";
-  }
+  int value = 255 - info.timeSinceLastTap();
+  if (value < 20)
+    value = 20;
+  int value2 = 20+info.getLocalActivity() * 200;
+  if (value2 > 255) value2 = 255;
+  if (value < value2)
+    value = value2;
+  lights.setAllPixelHSVs(hue,255,value);
+  //  
+  //    uint16_t length = (getAnimationMillis() / 10) % (info.numLEDs*2);
+  //    if (length >= info.numLEDs)
+  //      length = info.numLEDs*2-1-length;
+  //    if (length >=  info.numLEDs | length < 0)
+  //      length = 0;
+  //    for(int i = 0; i < length; i++)
+  //      lights.setPixelColor(i, 64, 64, 64);
+}
+
+char * WWPAnimationTest:: name() {
+  return "WWPAnimationTest";
+}
+
