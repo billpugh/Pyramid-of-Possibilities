5dbe6126f61ec048fd0dca8791d64e021cde4735
Bill Pugh <pugh@cs.umd.edu>
2014-08-02 17:47:41 -0400

    make led positions available

--- Arduino/PyramidOfPossibilities/ledPositions.h
+++ Arduino/PyramidOfPossibilities/ledPositions.h
@@ -9,4 +9,8 @@
 #ifndef __PlatformCode__ledPositions__
 #define __PlatformCode__ledPositions__
 
+#include <stdint.h>
+
+void getLEDPosition(uint8_t led, int16_t &x, int16_t &y);
+
 #endif /* defined(__PlatformCode__ledPositions__) */
