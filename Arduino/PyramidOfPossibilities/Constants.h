1f05cab99613613873fad83066cdfc6f82a4843e
Danny Ricciotti <dan.ricciotti@gmail.com>
2014-08-02 13:44:56 -0400

    Moved location of Constants.h/cpp

---
+++ Arduino/PyramidOfPossibilities/Constants.h
@@ -0,0 +1,15 @@
+//
+//  Constants.h
+//
+//  Created by Danny.
+//  Copyright (c) 2014 RN. All rights reserved.
+//
+
+#ifndef CONSTANTS_PYRAMID_OF_POP
+#define CONSTANTS_PYRAMID_OF_POP
+
+#define DEBUG true
+
+
+
+#endif /** CONSTANTS_PYRAMID_OF_POP */
