7f3cca2096b63c1c6248b94e644e5ad965622099
Bill Pugh <pugh@cs.umd.edu>
2014-08-02 17:46:50 -0400

    Updates

--- Arduino/PyramidOfPossibilities/RNEEPROM.cpp
+++ Arduino/PyramidOfPossibilities/RNEEPROM.cpp
@@ -7,3 +7,30 @@
 //
 
 #include "RNEEPROM.h"
+#include <EEPROM.h>
+
+void readFromEEPROM(uint8_t version, uint16_t sz, uint8_t *p) {
+     uint8_t storedVersion = EEPROM.read(0);
+    if (version != storedVersion) {
+        return;
+    }
+    uint16_t storedSize = EEPROM.read(1) << 8 | EEPROM.read(2);
+    
+    if (sz != storedSize) {
+        return;
+    }
+    for(int i = 0; i < sz; i++)
+        p[i] = EEPROM.read(3+i);
+    
+}
+
+
+void writeTo(uint8_t version, uint16_t sz, uint8_t *p) {
+    EEPROM.write(0, version);
+    EEPROM.write(1, sz>>8);
+    EEPROM.write(2, sz);
+
+    for(int i = 0; i < sz; i++)
+        EEPROM.write(3+i,p[i]);
+    
+}
--- Arduino/PyramidOfPossibilities/RNEEPROM.h
+++ Arduino/PyramidOfPossibilities/RNEEPROM.h
@@ -6,9 +6,13 @@
 //  Copyright (c) 2014 RN. All rights reserved.
 //
 
-#ifndef __PlatformCode__RNEEPROM__
-#define __PlatformCode__RNEEPROM__
+#ifndef __RNEEPROM__
+#define __RNEEPROM__
 
-#include <iostream>
 
-#endif /* defined(__PlatformCode__RNEEPROM__) */
+
+void readFromEEPROM(char version, short sz, char *p);
+// void writeTo(uint8_t version, uint16_t sz, uint8_t *p)
+// void writeTo(char version, short sz, char *p)
+
+#endif /* defined(__RNEEPROM__) */
