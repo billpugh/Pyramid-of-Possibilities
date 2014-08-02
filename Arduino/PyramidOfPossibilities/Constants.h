625aa700718605f3e506543a869252d3fe671907
Danny Ricciotti <dan.ricciotti@gmail.com>
2014-08-02 14:05:30 -0400

    Moved build defines such as DEBUG, FULL_STRIP, and LEDs to the Constants.h file.

--- Arduino/PyramidOfPossibilities/Constants.h
+++ Arduino/PyramidOfPossibilities/Constants.h
@@ -8,6 +8,22 @@
 #ifndef CONSTANTS_PYRAMID_OF_POP
 #define CONSTANTS_PYRAMID_OF_POP
 
+// To enable DEBUG mode, uncomment the line below
 #define DEBUG
+
+// To test without using the full strip of LEDs, uncomment the line below and set the number of LEDs you want, as well as the index of the 1st LED.
+#define FULL_STRIP
+
+
+// Set values for the # of LEDs and 1st LED Index
+#ifdef FULL_STRIP
+// DO NOT MODIFY THESE VALUES
+const int LEDs = 220;
+const int FIRST_LED = 10;
+#else
+// Modify these values for testing with a non-full strip.
+const int LEDs = 60;
+const int FIRST_LED = 0;
+#endif
 
 #endif /** CONSTANTS_PYRAMID_OF_POP */
