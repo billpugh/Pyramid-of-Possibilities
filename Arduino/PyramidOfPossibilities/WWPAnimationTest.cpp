95e143f4ee6892401cc56238d623ef5bf79d39b0
Bill Pugh <pugh@cs.umd.edu>
2014-08-03 11:42:26 -0400

    Tweaks to beacon animation

--- Arduino/PyramidOfPossibilities/WWPAnimationTest.cpp
+++ Arduino/PyramidOfPossibilities/WWPAnimationTest.cpp
@@ -10,32 +10,25 @@
 
 void WWPAnimationTest::paint(RNLights & lights) {
     
-    int firstLED = 1000;
-    int lastLED = -1;
     
     float angle = getAnimationMillis()/2000.0;
     
     for(int i = 0; i < lights.getNumPixels(); i++) {
         float a = info.getGlobalAngle(i) - angle;
+        
+        if (1) {
+        a = a - roundf(a);
         if (a < 0) a = -a;
-        a = a - floor(a);
-        if (a > 0.5) a = 1.0-a;
         if (a < 0.05) {
-
-        lights.setPixelColor( i, 255, 0, 0);
-            if (firstLED > i)
-                firstLED =i;
-            if (lastLED < i)
-                lastLED = i;
-                
+            lights.setPixelHSV(i, 0, 255, 255-a*5000);
+        }
+        } else {
+            a = a - floorf(a);
+            lights.setPixelHSV(i, a*256, 255, 128);
         }
         
     }
-    if (firstLED <= lastLED)
-        info.printf("Angle %f, LEDs %d ... %d\n", angle, firstLED, lastLED);
-    else
-        info.printf("Angle %f, no LEDs\n", angle);
-    
+    info.showActivityWithSparkles(lights);
     
     
 }
