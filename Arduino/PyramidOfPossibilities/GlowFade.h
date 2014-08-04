ec36b1533501f75c768b56718af10ac1a4fa1c36
Bill Pugh <pugh@cs.umd.edu>
2014-08-04 14:09:42 -0400

    Formatting

--- Arduino/PyramidOfPossibilities/GlowFade.h
+++ Arduino/PyramidOfPossibilities/GlowFade.h
@@ -12,13 +12,13 @@
 #include "RNGradient.h"
 
 struct GlowFadeParameters {
-         uint8_t gradiantSkip = 69 ;
-     uint16_t period = 4000;
-    float fractionUp = 0.75;
+    uint8_t gradiantSkip = 69 ;
+    uint16_t period = 4000;
+    float fractionUp = 0.5;
 
     RNGradient gradient = RNGradient(1, RNGradientWrap,
-                                   0x00ffff, 0xffffff);
-    
+                                     0x00ffff, 0xffffff);
+
 };
 
 
@@ -27,11 +27,11 @@ class GlowFade : public RNAnimation {
 public:
     GlowFade(RNInfo & info, unsigned long animationStartMillis)
     : RNAnimation(info, animationStartMillis, sizeof(GlowFadeParameters), &parameters) {
-        
+
     };
     virtual void paint(RNLights & lights);
     virtual const char * name();
-    
+
     void getLinear(float pos, uint8_t &value);
     GlowFadeParameters parameters;
 
