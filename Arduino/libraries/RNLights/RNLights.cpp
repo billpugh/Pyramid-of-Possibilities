b7189b98a3bd9f043913c138f41acb55e1af7da8
Bill Pugh <pugh@cs.umd.edu>
2014-08-10 23:37:50 -0400

    Add setPixelColorMax that takes a 32 bit color

--- Arduino/libraries/RNLights/RNLights.cpp
+++ Arduino/libraries/RNLights/RNLights.cpp
@@ -175,6 +175,11 @@ void RNLights::setPixelColor(uint16_t pixel, uint32_t colorRGB) {
   pixels[pixel+2] = (uint8_t) colorRGB;
 }
 
+void RNLights::setPixelColorMax(uint16_t pixel, uint32_t colorRGB) {
+    setPixelColorMax(pixel,
+        (colorRGB >> 16), (colorRGB >> 8),  colorRGB);
+}
+
 uint32_t RNLights::getPixelColor(uint16_t pixel) {
   pixel = 3*((pixel+offset) % numPixels);
   return (pixels[pixel] << 16)
