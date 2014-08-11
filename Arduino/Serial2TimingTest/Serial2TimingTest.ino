daeee3aff119795b9e436240065149c5aba982cc
Bill Pugh <pugh@cs.umd.edu>
2014-08-11 19:11:29 -0400

    Timing tests

--- Arduino/Serial2TimingTest/Serial2TimingTest.ino
+++ Arduino/Serial2TimingTest/Serial2TimingTest.ino
@@ -1,5 +1,8 @@
 
 
+
+
+// RS485 on UART1
 // RS485 on UART1
 #define rxPin 9
 #define txPin 10
@@ -10,42 +13,50 @@ void setup()
   delay(2000);
   pinMode(led, OUTPUT);  
   Serial.begin(9600);
-  Serial.println("Hello, this is Serial2ReceiveTest");
-  pinMode(rxPin, INPUT_PULLUP);
+  Serial.println("Hello, this is Serial2TimingTest");
+   pinMode(rxPin, INPUT_PULLUP);
   pinMode(txPin, OUTPUT); 
   Serial2.begin(9600); 
 
   // RS485 RTS handshake signal setup
   pinMode(ctsPin, OUTPUT);
   CORE_PIN22_CONFIG = PORT_PCR_MUX(3);	// Set UART1_RTS function (ALT3, see page 209)
-  UART1_MODEM = 0x06;			// Set TXRTSE enable RTS on transmit with active HIGH
-
+  UART1_MODEM = 0x06;	
 }
 
-extern volatile uint32_t rx_start_ms;
 
+extern volatile uint32_t rx_start_ms;
+uint32_t lastData = 0;
 void loop() {
+
   int available = Serial2.available();
   if (available > 0) {
     digitalWrite(led, HIGH);
-    Serial.print("Available: " );
+    uint32_t received_at = rx_start_ms;
+    Serial.print("Available: ");
     Serial.println(available);
-    Serial.print("at: " );
-    Serial.println(rx_start_ms);
-    Serial.print("now: " );
+    Serial.print("Arrived at: ");
+    Serial.println(received_at);
+    Serial.print("Now at: ");
     Serial.println(millis());
-    while (Serial2.available() > 0) {
-      Serial.print(Serial2.read());
-    }
+    Serial.print("period: ");
+    Serial.println(received_at - lastData);
+    lastData = received_at;
+
+     while  (Serial2.available() > 0) {
+      Serial.print((char)Serial2.read());
+    } 
+   
+
     Serial.println();
-    delay(300);
   }
 
 
-  delay(100);
+  delay(200);
   digitalWrite(led, LOW);
   delay(100);
 }
+
 
 
 
