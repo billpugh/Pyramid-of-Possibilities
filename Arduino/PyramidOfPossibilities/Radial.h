5c51dcf9d0b2d33e796f4994ad4d62ec0a30daad
Danny Ricciotti <dan.ricciotti@gmail.com>
2014-08-16 01:10:20 -0400

    Created 4 variations to parameters for Radial.

--- Arduino/PyramidOfPossibilities/Radial.h
+++ Arduino/PyramidOfPossibilities/Radial.h
@@ -29,15 +29,51 @@ enum RadialCompressionType {
     RadialCompressAll = 0xFF
 };
 
+
+// Heart Beat
 struct RadialParameters {
     float thickness = 0.05; // portion of entire radius
     RNGradient gradientInside = RNGradient(0, RNGradientCapped, 0x000000, 0xff0000);
-    RNGradient gradientShell = RNGradient(0, RNGradientCapped, 0xFF0000, 0xFF8080);
-    RNGradient gradientOutside = RNGradient(0, RNGradientCapped, 0x000002, 0x000002);
-    EasingMode easingMode = EaseInOut;
-    CurveType curveType = CurveTypeQuadratic;
+    RNGradient gradientShell = RNGradient(0, RNGradientCapped, 0xFF0000, 0xFF0000);
+    RNGradient gradientOutside = RNGradient(0, RNGradientCapped, 0x040000, 0x040000);
+    EasingMode easingMode = EaseIn;
+    CurveType curveType = CurveTypeBounce;
     uint8_t compress = RadialCompressInner | RadialCompressOuter;
 };
+
+// Chaos!
+//struct RadialParameters {
+//    float thickness = 0.3; // portion of entire radius
+//    RNGradient gradientInside = RNGradient(1, RNGradientBounce, 0x00FFFF, 0xFFFFFF);
+//    RNGradient gradientShell = RNGradient(1, RNGradientBounce, 0xFFFF00, 0xFFFF00);
+//    RNGradient gradientOutside = RNGradient(1, RNGradientBounce, 0x00FFFF, 0xFFFFFF);
+//    EasingMode easingMode = EaseOut;
+//    CurveType curveType = CurveTypeLinear;
+//    uint8_t compress = RadialCompressInner | RadialCompressOuter;
+//};
+
+// Rainbow Heart
+//struct RadialParameters {
+//    float thickness = 0.00; // portion of entire radius
+//    RNGradient gradientInside = RNGradient(1, RNGradientBounce, 0x00FFFF, 0xFFFF00);
+//    RNGradient gradientShell = RNGradient(1, RNGradientBounce, 0x00FFFF, 0xFFFFFF);
+//    RNGradient gradientOutside = RNGradient(1, RNGradientBounce, 0x00FFFF, 0xFFFFFF);
+//    EasingMode easingMode = EaseOut;
+//    CurveType curveType = CurveTypeExpo;
+//    uint8_t compress = 0;
+//};
+
+// Blue Sparkle
+//struct RadialParameters {
+//    float thickness = 0.2; // portion of entire radius
+//    RNGradient gradientInside = RNGradient(0, RNGradientBounce, 0x0088FF, 0x0088FF);
+//    RNGradient gradientShell = RNGradient(0, RNGradientBounce, 0x0088FF, 0x000080);
+//    RNGradient gradientOutside = RNGradient(0, RNGradientBounce, 0x000080, 0x000080);
+//    EasingMode easingMode = EaseOut;
+//    CurveType curveType = CurveTypeBounce;
+//    uint8_t compress = RadialCompressOuter;
+//};
+
 
 class Radial : public RNAnimation {
 public:
