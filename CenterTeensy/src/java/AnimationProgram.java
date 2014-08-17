00127ea6c9b008e02cc230a46dd758250a362e1b
Bill Pugh <pugh@cs.umd.edu>
2014-08-17 08:31:47 -0400

    Synchronize animation program list

--- CenterTeensy/src/java/AnimationProgram.java
+++ CenterTeensy/src/java/AnimationProgram.java
@@ -1,43 +1,40 @@
 
 public enum AnimationProgram {
 
-	 e_LiftOff,
-	    e_Beacon,
-	    // Branden's animations
-	    e_Kittens,
+    e_Mute,
 
-	    e_FlashEcho,
+    // Animations defined by Bill Pugh (WWP)
+    e_ChangingColor,
+    e_CircularGradient,
+    e_ColorWorms,
+    e_Chasers,
+    e_FlashAttack,
+    e_Snow,
+    e_Swirl,
+    e_GlowFade,
+    e_LiftOff,
+    e_Beacon,
+    e_WWPAnimationTest,
+    e_ActivityLevelAnimation,
+    e_SideEffects,
 
-	    e_Radial,
-	    e_Mute,
+    // Danny's animations.
+    e_Qbert,   // RGB simple
+    e_DRAnimation1,   // RNBeam
+    e_FlashEcho,
+    e_Radial,
 
+    // Antoine's animations
+    e_Flashes,
+    e_AVEAnimation1,
+    e_MovingFaces,
+    e_Explosion,
 
-	    e_GlowFade,
+    // Branden's animations
+    e_Kittens,
 
-	    e_WWPAnimationTest,
-	    e_ActivityLevelAnimation,
-
-	    e_SideEffects,
-
-	    // Animations defined by Bill Pugh (WWP)
-	    e_ChangingColor,
-	    e_CircularGradient,
-
-	    e_ColorWorms,
-	    e_Chasers,
-	    e_FlashAttack,
-	    e_Snow,
-	    e_Swirl,
-
-	    // Danny's animations.
-	    e_Qbert,   // RGB simple
-	    e_DRAnimation1,   // RNBeam
-
-	    // Antoine's animations
-	    e_Flashes,
-	    e_MovingFaces,
-	    e_Explosion;
-
+    // meelows animations
+    e_MMAnimation1;         // strip color cycles through HSV, tapping activity brightens, no activity fades out
     
     
     
