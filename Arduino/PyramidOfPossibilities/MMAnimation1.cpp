6238234cc15570ac9cf9f79a1bd49efb867108a9
Bill Pugh <pugh@cs.umd.edu>
2014-08-17 10:37:52 -0400

    Updated Milo's animation to use: 	logarithmic fadeout with halflife 	ease in/out curve for brightness 	gradient for color

--- Arduino/PyramidOfPossibilities/MMAnimation1.cpp
+++ Arduino/PyramidOfPossibilities/MMAnimation1.cpp
@@ -7,6 +7,7 @@
 //
 
 #include "MMAnimation1.h"
+#include <math.h>
 
 #define MIN_BRIGHTNESS 20
 #define MAX_BRIGHTNESS 250
@@ -14,54 +15,41 @@
 
 #define DEBUG true  // print msgs over serial
 
-static uint8_t brightness = MIN_BRIGHTNESS;
-static unsigned long lastActivity=0;
+const float logOneHalf = -.6931471806; // log(1/2);
 
-void MMAnimation1::paint(RNLights & lights) 
+void MMAnimation1::paint(RNLights & lights)
 {
-  // flash on value if there was a tap, if not use brightness for value
-  int value = 255 - info.timeSinceLastTap();
-  if (value < 20)
-    value = 20;
-  if (value < brightness)
-    value = brightness;	
-	
-  // hue is made from globaltime	
-  uint8_t hue = (info.getGlobalMillis() / 150) % 256;
 
-  lights.setAllPixelHSVs(hue,250,value);  
-  
-  // increase /decrease brightness:
-  if(info.getTaps() )
-  {
-    if( brightness<MAX_BRIGHTNESS ) brightness+=STEP_BRIGHTNESS;
-    if(DEBUG) info.printf("[MM1]: brightness=%i\n", brightness);
-	lastActivity = getAnimationMillis();
-    if(false)
-	{
-	  info.printf("[MM1]: numLEDs %i\n", info.numLEDs);
-	  // info.printf("[MM1]: tier    %i\n", info.tier);
-	  // info.printf("[MM1]: number  %i\n", info.number);
-	  info.printf("[MM1]: getTaps()           %u\n", info.getTaps());
-	  info.printf("[MM1]: timeSinceLastTap    %u\n", info.timeSinceLastTap());
-	  info.printf("[MM1]: getLocalActivity    %f\n", info.getLocalActivity());
-	  info.printf("[MM1]: getGlobalActivity   %f\n", info.getGlobalActivity());
-//	  info.printf("[MM1]: getGlobalRadius    %u\n", info.getGlobalRadius(0));
-	  info.printf("[MM1]: getGlobalMillis    %u\n", info.getGlobalMillis());
-	  info.printf("[MM1]: getRandom          %i\n", info.getRandom(1000));
-	  info.printf("[MM1]: getRandomPixel     %i\n", info.getRandomPixel());
-	}
-  }
-  
-  // last activity longer than 1s ago?
-  if( getAnimationMillis() - lastActivity > 1000 )
-  {
-    if( brightness>MIN_BRIGHTNESS ) brightness-= STEP_BRIGHTNESS;
-    if(DEBUG) info.printf("[MM1]: brightness=%i\n", brightness);
-	lastActivity = getAnimationMillis();
-  }
+    uint32_t now = getAnimationMillis();
+    uint32_t since = now - lastUpdate;
+    lastUpdate = now;
+
+    double logFade = logOneHalf * since / parameters.halfLifeMillis;
+    brightness *= exp(logFade);
+
+    if (info.getTaps()) {
+        brightness += parameters.increaseOnTap;
+        // brightness is allowed to go above 1.0
+    }
+
+    float pulseBrightness =
+    1.0 - info.timeSinceLastTap()/parameters.pulseWidth;
+    if (pulseBrightness < 0)
+        pulseBrightness = 0.0;
+
+    AHEasingFunction easingFunction = getEasingFunction(parameters.easingMode, parameters.curveType);
+    float b = easingFunction(fmin(1.0,fmax(brightness,pulseBrightness)));
+
+    int finalBrightness = parameters.minimumBrightness + b * (parameters.maxBrightness - parameters.minimumBrightness);
+
+    // gradiant uses cycle value
+    uint8_t gradientPosition = getAnimationCycles()*256;
+    uint32_t color = parameters.gradient.getColor(gradientPosition);
+
+    lights.setAllPixelColors(color);
+    lights.setBrightness(finalBrightness);
 }
 
-char * MMAnimation1:: name() {
+const char * MMAnimation1:: name() {
 	return "Traces(MM1)";
 }
--- Arduino/PyramidOfPossibilities/MMAnimation1.h
+++ Arduino/PyramidOfPossibilities/MMAnimation1.h
@@ -1,5 +1,5 @@
 //
-//  DRAnimation0.h
+//  MMAnimation1.h
 //  PlatformData
 //
 //  Created by Meelow based on Bills and Dannys work
@@ -10,16 +10,38 @@
 #define __PlatformData__MMAnimation1__
 
 #include "RNAnimation.h"
+#include "RNGradient.h"
+#include "easingSelect.h"
+
+struct MMAnimation1Parameters {
+
+    uint32_t halfLifeMillis = 10000;
+    float increaseOnTap = 0.1;
+    float pulseWidth = 250;
+
+    RNGradient gradient =
+    RNGradient(1, RNGradientWrap, 0x00ffff, 0xffffff);
+    EasingMode easingMode = EaseIn;
+    CurveType curveType = CurveTypeLinear;
+    uint8_t minimumBrightness = 2;
+    uint16_t maxBrightness = 256;
+
+};
 
 class MMAnimation1 : public RNAnimation {
 private:
 
 public:
     MMAnimation1(RNInfo & info, AnimationInfo animationInfo)
-    : RNAnimation(info, animationInfo) {};
-	// MMAnimation1(RNInfo & info, unsigned long animationStartMillis);
+    : RNAnimation(info, animationInfo, sizeof(MMAnimation1Parameters), &parameters) {};
+
     virtual void paint(RNLights & lights);
-    virtual char * name();
+    virtual const char * name();
+private:
+    double brightness = 0.0;
+    MMAnimation1Parameters parameters;
+    uint32_t lastUpdate = 0;
+
 
 };
 
