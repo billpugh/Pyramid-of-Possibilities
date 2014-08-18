7a46fc8f5288354fb4a51f77050b0180d23f91ba
Bill Pugh <pugh@cs.umd.edu>
2014-08-18 18:58:26 -0400

    Send only needed bytes for animations and effects

--- CenterTeensy/src/java/Animation.java
+++ CenterTeensy/src/java/Animation.java
@@ -1,128 +1,146 @@
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
-
 public class Animation {
-    
-    enum TweakKind { SIGNED, UNSIGNED, CYCLIC, CYCLIC_UNARY;
-     byte add(int currentValue, int offset) {
-         switch (this) {
-         case SIGNED:
-             currentValue += offset;
-             if (currentValue < -128) return -128;
-             else if (currentValue > 127)
-                 return 127;
-             return (byte) currentValue;
-         case UNSIGNED:
-             currentValue = currentValue & 0xff;
-             currentValue += offset;
-             if (currentValue < 0) return 0;
-             else if (currentValue > 255)
-                 return (byte)255;
-             return (byte) currentValue;
-         case CYCLIC:
-         case CYCLIC_UNARY:
-        	 return (byte)(currentValue + offset);
-         }
-         return (byte)(currentValue + offset);
-     }
+
+    enum TweakKind {
+        SIGNED, UNSIGNED, CYCLIC, CYCLIC_UNARY;
+        byte add(int currentValue, int offset) {
+            switch (this) {
+            case SIGNED:
+                currentValue += offset;
+                if (currentValue < -128)
+                    return -128;
+                else if (currentValue > 127)
+                    return 127;
+                return (byte) currentValue;
+            case UNSIGNED:
+                currentValue = currentValue & 0xff;
+                currentValue += offset;
+                if (currentValue < 0)
+                    return 0;
+                else if (currentValue > 255)
+                    return (byte) 255;
+                return (byte) currentValue;
+            case CYCLIC:
+            case CYCLIC_UNARY:
+                return (byte) (currentValue + offset);
+            }
+            return (byte) (currentValue + offset);
+        }
     }
 
-	  class TweakStatus {
-		    final byte tweakValue;
-	    	final  double cycles;
-	    	final double tweakCount;
-	    	// Relative to animationStartTime
-	    	 final  int lastTweakAt;
-	    	 public TweakStatus(byte tweakValue) {
-	    		 this(tweakValue, 0, 0, 0);
-	    	 }
-	    	 
-	    	 public int getIntValue() {
-	    		 if (program.tweakKind == TweakKind.SIGNED)
-	    			 return tweakValue;
-	    		 return 0xff & tweakValue;
-	    	 }
+    class TweakStatus {
+        final byte tweakValue;
+        final double cycles;
+        final double tweakCount;
+        // Relative to animationStartTime
+        final int lastTweakAt;
 
-			private TweakStatus(byte cpm, double cycles, double tweakCount, int computedAt) {
-				this.tweakValue = cpm;
-				this.cycles = cycles;
-				this.tweakCount = tweakCount;
-				this.lastTweakAt = computedAt;
-			}
-			
-			TweakStatus tweakUp() {
-				int at = getAnimationMillis();
-				if (at < lastTweakAt) throw new IllegalArgumentException();
-				double newTweakCount = tweakCount+ 1 - (at-lastTweakAt)/500.0;
-				if (newTweakCount < 0)
-					newTweakCount = 0;
-				else if (newTweakCount > 8)
-					newTweakCount = 8;
-				double newCycleCount = cycles + tweakValue * (at-lastTweakAt)/120000.0;
-				int increment = 1;
-				if (program.tweakKind !=TweakKind.CYCLIC_UNARY && newTweakCount > 5)
-				    increment = 4;
+        public TweakStatus(byte tweakValue) {
+            this(tweakValue, 0, 0, 0);
+        }
 
-				return new TweakStatus(program.tweakKind.add(tweakValue, increment), newCycleCount, newTweakCount, at);
-			}
-			TweakStatus tweakDown() {
-				int at =  getAnimationMillis();
-				if (at < lastTweakAt) throw new IllegalArgumentException();
-				double newTweakCount = tweakCount+ 1 - (at-lastTweakAt)/500.0;
-				if (newTweakCount < 0)
-					newTweakCount = 0;
-				else if (newTweakCount > 8)
-					newTweakCount = 8;
-				double newCycleCount = cycles + tweakValue * (at-lastTweakAt)/120000.0;
-				int decrement = -1;
-                if (program.tweakKind !=TweakKind.CYCLIC_UNARY && newTweakCount > 5)
-                    decrement = -4;
-				
-			    return new TweakStatus(program.tweakKind.add(tweakValue, decrement), newCycleCount, newTweakCount, at);
-	            
-			}
-			
-	    }
-	 
+        public int getIntValue() {
+            if (program.tweakKind == TweakKind.SIGNED)
+                return tweakValue;
+            return 0xff & tweakValue;
+        }
+
+        private TweakStatus(byte cpm, double cycles, double tweakCount,
+                int computedAt) {
+            this.tweakValue = cpm;
+            this.cycles = cycles;
+            this.tweakCount = tweakCount;
+            this.lastTweakAt = computedAt;
+        }
+
+        TweakStatus tweakUp() {
+            int at = getAnimationMillis();
+            if (at < lastTweakAt)
+                throw new IllegalArgumentException();
+            double newTweakCount = tweakCount + 1 - (at - lastTweakAt) / 500.0;
+            if (newTweakCount < 0)
+                newTweakCount = 0;
+            else if (newTweakCount > 8)
+                newTweakCount = 8;
+            double newCycleCount = cycles + tweakValue * (at - lastTweakAt)
+                    / 120000.0;
+            int increment = 1;
+            if (program.tweakKind != TweakKind.CYCLIC_UNARY
+                    && newTweakCount > 5)
+                increment = 4;
+
+            return new TweakStatus(
+                    program.tweakKind.add(tweakValue, increment),
+                    newCycleCount, newTweakCount, at);
+        }
+
+        TweakStatus tweakDown() {
+            int at = getAnimationMillis();
+            if (at < lastTweakAt)
+                throw new IllegalArgumentException();
+            double newTweakCount = tweakCount + 1 - (at - lastTweakAt) / 500.0;
+            if (newTweakCount < 0)
+                newTweakCount = 0;
+            else if (newTweakCount > 8)
+                newTweakCount = 8;
+            double newCycleCount = cycles + tweakValue * (at - lastTweakAt)
+                    / 120000.0;
+            int decrement = -1;
+            if (program.tweakKind != TweakKind.CYCLIC_UNARY
+                    && newTweakCount > 5)
+                decrement = -4;
+
+            return new TweakStatus(
+                    program.tweakKind.add(tweakValue, decrement),
+                    newCycleCount, newTweakCount, at);
+
+        }
+
+    }
+
     final AnimationProgram program;
     final byte sequenceId;
-   
+
     final int startTime = BurnerTime.getGlobalTime();
     final int duration;
     volatile TweakStatus tweakStatus;
-    byte [] parameters;
-    
-    
-    public void write(ByteBuffer buf ) {
-        buf.put((byte)program.ordinal());
+    byte[] parameters;
+
+    public void write(ByteBuffer buf, boolean effect) {
+        buf.put((byte) program.ordinal());
         buf.put(sequenceId);
         buf.putInt(startTime);
-        buf.putInt(duration);
+        if (effect)
+            buf.putInt(duration);
         TweakStatus cycles = getTweakStatus();
-        buf.putFloat((float)cycles.cycles);
-        buf.putInt(cycles.lastTweakAt);
+        if (!effect) {
+            buf.putFloat((float) cycles.cycles);
+            buf.putInt(cycles.lastTweakAt);
+        }
         buf.put(cycles.tweakValue);
         buf.putShort(byteLength());
         if (parameters.length > 0)
             buf.put(parameters);
     }
-    
-    
+
     static byte nextSequenceId = 0;
 
     public void tweakUp() {
-    	tweakStatus = tweakStatus.tweakUp();
+        tweakStatus = tweakStatus.tweakUp();
     }
+
     public void tweakDown() {
-    	tweakStatus = tweakStatus.tweakDown();
+        tweakStatus = tweakStatus.tweakDown();
     }
-    
+
     public int getAnimationMillis() {
-    	 return BurnerTime.getGlobalTime() - startTime;
+        return BurnerTime.getGlobalTime() - startTime;
     }
-    
-    public Animation(AnimationProgram program, byte initialTweakValue, int duration, byte[] parameters) {
+
+    public Animation(AnimationProgram program, byte initialTweakValue,
+            int duration, byte[] parameters) {
         this.program = program;
         this.sequenceId = nextSequenceId++;
         this.parameters = parameters;
@@ -131,44 +149,40 @@ public class Animation {
     }
 
     public Animation(AnimationProgram program) {
-       this(program, (byte)30, 0, new byte[0]);
+        this(program, (byte) 30, 0, new byte[0]);
     }
 
-    public  TweakStatus getTweakStatus() {
-    	return tweakStatus;
+    public TweakStatus getTweakStatus() {
+        return tweakStatus;
     }
 
     public short byteLength() {
-        int length = 17+parameters.length;
+        int length = 17 + parameters.length;
         if (length > Short.MAX_VALUE)
             throw new AssertionError();
         return (short) length;
-        
+
     }
 
-
-    
-
     public ByteBuffer getBytes() {
-        ByteBuffer buf = ByteBuffer.allocate(byteLength()+20);
+        ByteBuffer buf = ByteBuffer.allocate(byteLength() + 20);
         buf.order(ByteOrder.LITTLE_ENDIAN);
-        
+
         write(buf);
         buf.flip();
 
         if (buf.limit() != byteLength())
-            throw new AssertionError(
-                    String.format("limit is %d, bytelength is %d", 
-                            buf.limit(), byteLength()));
+            throw new AssertionError(String.format(
+                    "limit is %d, bytelength is %d", buf.limit(), byteLength()));
 
         return buf;
     }
-    
+
     public static void main(String args[]) {
         Animation a = new Animation(AnimationProgram.e_ActivityLevelAnimation);
         ByteBuffer buf = a.getBytes();
         System.out.println(a.byteLength());
         System.out.println(buf.limit());
-        
+
     }
 }
