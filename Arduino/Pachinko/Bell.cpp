a8d1851c17faa55679fe7286c746291fddc5911b
Bill Pugh <pugh@cs.umd.edu>
2015-06-04 06:59:58 -0400

    Describe which bell is turned on/off

--- Arduino/Pachinko/Bell.cpp
+++ Arduino/Pachinko/Bell.cpp
@@ -12,33 +12,37 @@
 
 
 Bell::Bell(Adafruit_MCP23017 & io, int pin, int durationOn, int durationOff)
-: io(io), pin(pin), durationOn(durationOn), durationOff(durationOff),
-enqueued(0), on(false), timeOff(0) {
-    
+  : io(io), pin(pin), durationOn(durationOn), durationOff(durationOff),
+    enqueued(0), on(false), timeOff(0) {
+
 }
 
 void Bell::begin() {
-    io.pinMode(pin, OUTPUT);
-    timeOff = millis();
+  io.pinMode(pin, OUTPUT);
+  timeOff = millis();
 }
 
 void Bell::update() {
-    if (on) {
-        if (millis() > timeOff) {
-            on = false;
-            io.digitalWrite(pin, 0);
-            Serial.println("Turning bell off");
-        }
-    } else if (enqueued > 0 && millis() > timeOff + durationOff) {
-        on = true;
-        enqueued--;
-        io.digitalWrite(pin, 1);
-        Serial.println("Turning bell on");
-        timeOff = millis() + durationOn;
+  if (on) {
+    if (millis() > timeOff) {
+      on = false;
+      io.digitalWrite(pin, 0);
+      Serial.print("Turning bell " );
+      Serial.print(pin);
+      Serial.println(" off");
     }
-    
+  } else if (enqueued > 0 && millis() > timeOff + durationOff) {
+    on = true;
+    enqueued--;
+    io.digitalWrite(pin, 1);
+    Serial.print("Turning bell " );
+    Serial.print(pin);
+    Serial.println(" on");
+    timeOff = millis() + durationOn;
+  }
+
 }
-void Bell::ring(int num){
-    enqueued+= num;
-    
+void Bell::ring(int num) {
+  enqueued += num;
+
 }
