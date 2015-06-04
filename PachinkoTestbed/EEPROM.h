f46c9189e99d504e50be383969b73d8ff5bada9a
Bill Pugh <pugh@cs.umd.edu>
2015-06-04 07:04:24 -0400

    add EEPROM library

--- PachinkoTestbed/EEPROM.h
+++ PachinkoTestbed/EEPROM.h
@@ -9,5 +9,22 @@
 #ifndef PachinkoTestbed_EEPROM_h
 #define PachinkoTestbed_EEPROM_h
 
+class EEPROM_class {
+public:
+    uint8_t read( int idx )              { return 0; }
+    void write( int idx, uint8_t val )   {}
+    //Functionality to 'get' and 'put' objects to and from EEPROM.
+    template< typename T > T &get( int idx, T &t ){
+        
+        return t;
+    }
+    
+    template< typename T > const T &put( int idx, const T &t ){
+    
+        return t;
+    }
+};
+
+extern EEPROM_class EEPROM;
 
 #endif
