7200abaf424ea20f1e57b8756be48f78dcf13a75
Bill Pugh <pugh@cs.umd.edu>
2015-06-04 07:02:23 -0400

    Add EEPROM library

--- Arduino/Pachinko/Pachinko.ino
+++ Arduino/Pachinko/Pachinko.ino
@@ -1,13 +1,16 @@
 #include <i2c_t3.h>
+#include <EEPROM.h>
 #include "Adafruit_MCP23017.h"
 #include "OctoWS2811.h"
 #include "FastLED.h"
 #include "PachinkoMain.h"
 
 
+
 Adafruit_MCP23017 mcp;
 
 void setup() {
+  delay(2000);
   setupMain();
   
 
