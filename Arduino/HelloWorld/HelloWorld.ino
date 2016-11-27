2f74a4fcb1f96b3a2e76ce9b1f756044a1b11d85
Bill Pugh <pugh@cs.umd.edu>
2016-11-27 18:32:15 -0500

    Updates to HelloWork for platformio

--- Arduino/HelloWorld/HelloWorld.ino
+++ Arduino/HelloWorld/HelloWorld.ino
@@ -1,3 +1,5 @@
+#include <Arduino.h>
+
 void setup() {
   // put your setup code here, to run once:
   delay(200);
@@ -7,5 +9,9 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+ Serial.print(millis());
+ Serial.print("  ");
+ 
+ delay(1000);
 }
+
